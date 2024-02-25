d029c26c3b4646d3fbfc26498814a0b89f7e67d3
FolkRattapol <160979437+FolkRattapol@users.noreply.github.com>
2024-02-25 08:54:18 +0000

    add line 4-8

--- hello.r
+++ hello.r
@@ -1,2 +1,8 @@
 # This is R programming.
 print("Hello World")
+
+# test export into github
+kmitl_filter <- kmitl %>%
+          select(firstname, lastname, age) %>%
+          filter(age = 22) %>%
+          arrange(firstname) 
